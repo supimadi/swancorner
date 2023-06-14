43db02b8b34afedc482de5b053bfca39d7c492c2
Lukman Hakim <hlukman156@gmail.com>
2023-06-14 14:37:40 +0700

    add new asset and scene

--- Assets/Dnk_Dev/HospitalHorrorPack/Script/PlayerController.cs
+++ Assets/Dnk_Dev/HospitalHorrorPack/Script/PlayerController.cs
@@ -53,9 +53,9 @@ public class PlayerController : MonoBehaviour
 
     private void MouseMove()
     {
-        turn.x += Input.GetAxis("Mouse X") * sensitivity;
+        turn.x += -Input.GetAxis("Mouse X") * sensitivity;
         turn.y += Input.GetAxis("Mouse Y") * sensitivity;
-        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        transform.localRotation = Quaternion.Euler(turn.y, -turn.x, 0);
 
     }
 
@@ -82,11 +82,11 @@ public class PlayerController : MonoBehaviour
 
     private void MyInput()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = -Input.GetAxisRaw("Vertical");
+        verticalInput = Input.GetAxisRaw("Horizontal");
 
         // when to jump
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
 
@@ -102,11 +102,10 @@ public class PlayerController : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         // on ground
-        if(grounded)
+        if (grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
         // in air
-        else if(!grounded)
+        else if (!grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
     }
 
@@ -115,7 +114,7 @@ public class PlayerController : MonoBehaviour
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         // limit velocity if needed
-        if(flatVel.magnitude > moveSpeed)
+        if (flatVel.magnitude > moveSpeed)
         {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
